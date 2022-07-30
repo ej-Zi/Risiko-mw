07faadf82fea72ba065cf3cf836856bedeed21bf
Eva J. Zimmermann <eva@fichtensepp.de>
2022-07-30 12:40:37 +0200

    assests-hinzugefuegt

--- eclipseProjekt/src/risiko/TestMain.java
+++ eclipseProjekt/src/risiko/TestMain.java
@@ -2,19 +2,56 @@
 
 package risiko;
 
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
 public class TestMain {
 
 	public static void main(String[] args) {
 		
-		Spieler s1 = new Spieler();
+		//Karte als Bild anzeigen:
+		File image = new File("assets\\Risiko-Karte.jpg");
+		BufferedImage map = null;
+		try {
+			map = ImageIO.read(image);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		s1.setEinheiten(10);
-		System.out.println(s1.getEinheiten());
+		//resize image: (muss im Verhältnis zur Bildschirmgröße angepasst werden)
+		try {
+		map = resizeImage(map, 1654, 1169);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 		
-		s1.setEinheiten(s1.getEinheiten() + 3);
-		System.out.println(s1.getEinheiten());
 		
+		
+		
+		JLabel mapLabel = new JLabel(new ImageIcon(map));
+		JPanel jPanel = new JPanel();
+		jPanel.add(mapLabel);
+	
+		JFrame f = new JFrame();
+		f.setSize(new Dimension(map.getWidth(), map.getHeight()));
+		f.add(jPanel);
+		f.setVisible(true);
 		
 	}
-
+		//Bitte nicht löschen:
+	    public static BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
+			Image resultingImage = originalImage.getScaledInstance(targetWidth, targetHeight, Image.SCALE_DEFAULT);
+			BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+			outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+			return outputImage;
 }
+	
+}
+
+
